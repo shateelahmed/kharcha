6101a3e0ce0e6169c991495d7fdf74d8f4264514
Shateel Ahmed <shateel.ahmed@brainstation23.com>
2022-08-13 17:37:07 +0600

    feat: implement error handling in expense controller

--- src/backend/v1/controllers/expenseController.js
+++ src/backend/v1/controllers/expenseController.js
@@ -1,63 +1,112 @@
 const ExpenseService = require('../services/expenseService');
 
 const getAllExpenses = (_req, res) => {
-    const expenses = ExpenseService.getAllExpenses();
-    res.send({
-        data: expenses
-    });
+    try {
+        const expenses = ExpenseService.getAllExpenses();
+        res.send({
+            data: expenses
+        });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({
+                message: error?.message || 'Something went wrong',
+                error: error ? error : null,
+            });
+    }
 }
 
 const getOneExpense = (req, res) => {
-    const { expenseId } = req.params;
-    const expense = ExpenseService.getOneExpense(expenseId);
-    res.send({
-        data: expense
-    });
+    try {
+        const { expenseId } = req.params;
+        const expense = ExpenseService.getOneExpense(expenseId);
+        res.send({
+            data: expense
+        });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({
+                message: error?.message || 'Something went wrong',
+                error: error ? error : null,
+            });
+    }
 }
 
 const createOneExpense = (req, res) => {
-    const { body } = req;
+    try {
+        const { body } = req;
 
-    if (
-        !body.item
-        || !body.item_type
-        || !body.price
-    ) {
-        return res.status(422).send({
-            message: "Invalid input"
-        });
+        if (
+            !body.item
+            || !body.item_type
+            || !body.price
+        ) {
+            res
+                .status(422)
+                .send({
+                    message: "Invalid input"
+                });
+        }
+
+        const newExpense = {
+            item: body.item,
+            item_type: body.item_type,
+            price: body.price,
+            purchased_at: body.purchased_at
+        };
+
+        const createdExpense = ExpenseService.createOneExpense(newExpense);
+        res
+            .status(201)
+            .send({
+                data: createdExpense
+            });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({
+                message: error?.message || 'Something went wrong',
+                error: error ? error : null,
+            });
     }
-
-    const newExpense = {
-        item: body.item,
-        item_type: body.item_type,
-        price: body.price,
-        purchased_at: body.purchased_at
-    };
-
-    const createdExpense = ExpenseService.createOneExpense(newExpense);
-    res.status(201).send({
-        data: createdExpense
-    });
 }
 
 const updateOneExpense = (req, res) => {
-    const {
-        body,
-        params: { expenseId }
-    } = req;
-    const updatedExpense = ExpenseService.updateOneExpense(expenseId, body);
-    res.send({
-        data: updatedExpense
-    });
+    try {
+        const {
+            body,
+            params: { expenseId }
+        } = req;
+        const updatedExpense = ExpenseService.updateOneExpense(expenseId, body);
+        res.send({
+            data: updatedExpense
+        });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({
+                message: error?.message || 'Something went wrong',
+                error: error ? error : null,
+            });
+    }
 }
 
 const deleteOneExpense = (req, res) => {
-    const { params: { expenseId } } = req;
-    ExpenseService.deleteOneExpense(expenseId);
-    res.send({
-        message: "Expense deleted."
-    });
+    try {
+        const { params: { expenseId } } = req;
+        ExpenseService.deleteOneExpense(expenseId);
+        res.send({
+            message: "Expense deleted."
+        });
+    } catch (error) {
+        res
+            .status(error?.status || 500)
+            .send({
+                message: error?.message || 'Something went wrong',
+                error: error ? error : null,
+            });
+    }
 }
 
 module.exports = {
