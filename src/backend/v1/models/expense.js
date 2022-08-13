05ae60fa7adb4cf67d432201936a8735cc178304
Shateel Ahmed <shateel.ahmed@brainstation23.com>
2022-08-13 19:08:21 +0600

    feat: implement error handling in expense model

--- src/backend/v1/models/expense.js
+++ src/backend/v1/models/expense.js
@@ -2,53 +2,95 @@ const db = require('../../database/db.json');
 const { saveToDatabase } = require('../../database/utils');
 
 const getAllExpenses = () => {
-    return db.expenses;
+    try {
+        return db.expenses;
+    } catch (error) {
+        throw Object.assign(
+            new Error(error?.message || error),
+            { status: error?.status || 500 }
+        );
+    }
 }
 
 const getOneExpense = (expenseId) => {
-    const expense = db.expenses.find((exp) => exp.id == expenseId);
-    if (!expense) {
-        return;
+    try {
+        const expense = db.expenses.find((exp) => exp.id == expenseId);
+        if (!expense) {
+            return;
+        }
+        return expense;
+    } catch (error) {
+        throw Object.assign(
+            new Error(error?.message || error),
+            { status: error?.status || 500 }
+        );
     }
-    return expense;
 }
 
 const getNextExpenseId = () => {
-    const maxId = db.expenses.reduce((a, b) => a.id > b.id ? a : b).id;
-    return maxId + 1;
+    try {
+        const maxId = db.expenses.reduce((a, b) => a.id > b.id ? a : b).id;
+        return maxId + 1;
+    } catch (error) {
+        throw Object.assign(
+            new Error(error?.message || error),
+            { status: error?.status || 500 }
+        );
+    }
 }
 
 const createOneExpense = (newExpense) => {
-    db.expenses.push(newExpense);
-    saveToDatabase(db);
-    return newExpense;
+    try {
+        db.expenses.push(newExpense);
+        saveToDatabase(db);
+        return newExpense;
+    } catch (error) {
+        throw Object.assign(
+            new Error(error?.message || error),
+            { status: error?.status || 500 }
+        );
+    }
 }
 
 const updateOneExpense = (expenseId, changes) => {
-    const indexToUpdate = db.expenses.findIndex((expense) => expense.id == expenseId);
-    if (indexToUpdate === -1) {
-        return;
+    try {
+        const indexToUpdate = db.expenses.findIndex((expense) => expense.id == expenseId);
+        if (indexToUpdate === -1) {
+            return;
+        }
+
+        const updatedExpense = {
+            ...db.expenses[indexToUpdate],
+            ...changes,
+        };
+
+        db.expenses[indexToUpdate] = updatedExpense;
+        saveToDatabase(db);
+
+        return updatedExpense;
+    } catch (error) {
+        throw Object.assign(
+            new Error(error?.message || error),
+            { status: error?.status || 500 }
+        );
     }
-
-    const updatedExpense = {
-        ...db.expenses[indexToUpdate],
-        ...changes,
-    };
-
-    db.expenses[indexToUpdate] = updatedExpense;
-    saveToDatabase(db);
-
-    return updatedExpense;
 }
 
 const deleteOneExpense = (expenseId) => {
-    const indexToUpdate = db.expenses.findIndex((expense) => expense.id == expenseId);
-    if (indexToUpdate === -1) {
-        return;
-    }
+    try {
+        const indexToUpdate = db.expenses.findIndex((expense) => expense.id == expenseId);
+        if (indexToUpdate === -1) {
+            return;
+        }
 
-    db.expenses.splice(indexToUpdate, 1);
-    saveToDatabase(db);
+        db.expenses.splice(indexToUpdate, 1);
+        saveToDatabase(db);
+    } catch (error) {
+        throw Object.assign(
+            new Error(error?.message || error),
+            { status: error?.status || 500 }
+        );
+    }
 }
 
 module.exports = {
